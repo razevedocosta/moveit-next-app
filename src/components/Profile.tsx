7e1b1d281cea89de24069ba5983d21fcc8bf9a13
Rodrigo Azevedo <razevedocosta@gmail.com>
2021-02-26 10:53:31 -0400

    aplicação desenvolvida

---
+++ src/components/Profile.tsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import styles from "../styles/components/Profile.module.css";
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://avatars.githubusercontent.com/u/65239719?s=60&v=4" alt="Rodrigo Costa"/>
+
+            <div>
+                <strong>Rodrigo Costa</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level { level }
+                </p>
+            </div>
+        </div>
+    );
+}
